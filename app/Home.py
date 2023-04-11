c44d291fec0689f6a793c885161489147d007b48
Ashpreet Bedi <ashpreet@phidata.com>
2023-04-11 09:42:50 -0400

    update

--- app/Home.py
+++ app/Home.py
@@ -6,11 +6,8 @@ st.set_page_config(
 )
 
 st.markdown("### Select an App from the sidebar:")
-st.markdown("1. Stock Prediction: Predict stock price using time series forecasting")
-st.markdown("2. DuckGpt: Let GPT query your data using DuckDB")
+st.markdown("1. Time Series Forecast: Predict stock price using Prophet")
+st.markdown("2. Plotting Demo: Plot random numbers")
 st.markdown("\n")
-st.markdown(
-    "- built with [Streamlit](https://streamlit.io), [OpenAI](https://openai.com), [Langchain](https://langchain.readthedocs.io/en/latest/), [DuckDB](https://duckdb.org/) and [Phidata](https://docs.phidata.com)"  # noqa: E501
-)
 
 st.sidebar.success("Select an App from above")
